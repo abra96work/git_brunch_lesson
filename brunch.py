4620d4eb376da197fa11a37ec72785ff8753e6fa
Anton Abramov <abra96work@gmail.com>
2023-04-04 18:57:51 +0300

    add new tabs

--- brunch.py
+++ brunch.py
@@ -5,7 +5,7 @@ driver = webdriver.Chrome()
 
 driver.get('https://pikabu.ru/')
 
-tabs = ['Главная', 'Популярное', 'Новое', 'Сообщества', 'Каналы', 'Хабы', 'Картинки', 'Видео', 'Интересно']
+tabs = ['Главная', 'Популярное', 'Новое', 'Сообщества', 'Каналы', 'Хабы', 'Картинки', 'Видео', 'Интересно', 'new tab']
 for tab in tabs:
     driver.find_element_by_link_text(tab).click()
     assert tab in driver.title, f'Ошибка: не удалось перейти на вкладку {tab}'
