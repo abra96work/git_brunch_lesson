66b73bf4f9dc3361cb5942a47a288f54de5ebf0b
Anton Abramov <abra96work@gmail.com>
2023-04-04 18:54:38 +0300

    first commit1

---
+++ brunch.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+
+driver.get('https://pikabu.ru/')
+
+tabs = ['Главная', 'Популярное', 'Новое', 'Сообщества', 'Каналы', 'Хабы', 'Картинки', 'Видео', 'Интересно']
+for tab in tabs:
+    driver.find_element_by_link_text(tab).click()
+    assert tab in driver.title, f'Ошибка: не удалось перейти на вкладку {tab}'
+    time.sleep(2)
+
+driver.quit()
